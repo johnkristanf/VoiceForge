2b08f1c8018d770a3018450d046b3f9a2267e363
John Kristan <jkpogz09@gmail.com>
2024-01-25 16:26:03 +0800

    build: implement the auth route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+const Auth = React.lazy(() => import('./pages/Auth'))
 const TextSpeech = React.lazy(() => import("./pages/TextSpeech"))
 const VoiceCloning = React.lazy(() => import('./pages/VoiceCloning'))
 
@@ -13,6 +14,7 @@ const App = () => {
     <Suspense fallback={<div>Loading...</div>}>
 
          <Routes>
+                <Route path="/" Component={Auth} />
                 <Route path="text-speech" Component={TextSpeech} />
                 <Route path="voice-cloning" Component={VoiceCloning} />
          </Routes>
