0858f706338d22acac4f00e0b29a481a813a2c5f
John Kristan <jkpogz09@gmail.com>
2024-01-30 22:28:41 +0800

    refactor: Changes the delete request endpoint

--- client/src/services/http/delete/audio.ts
+++ client/src/services/http/delete/audio.ts
@@ -5,7 +5,9 @@ export const deleteAudio = async (audio_id: string, setdeletedID: any) => {
     console.log('audio_id', audio_id)
 
     try {
-        const response = await axios.delete(`http://localhost:800/api/audio/delete/${encodeURIComponent(audio_id)}`);
+        const response = await axios.delete(`http://localhost:800/api/audio/delete/${encodeURIComponent(audio_id)}`, {
+            withCredentials: true
+        });
         console.log('res', response)
         setdeletedID(response.data.DELETED)
 
