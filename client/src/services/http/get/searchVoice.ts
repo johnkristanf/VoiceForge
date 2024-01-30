dce4e9385c0c30599bc2d615c8234f154455f511
John Kristan <jkpogz09@gmail.com>
2024-01-30 22:39:37 +0800

    build: Implement the search request

---
+++ client/src/services/http/get/searchVoice.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+
+export const SearchVoice = async (voice_name: string) => {
+
+
+    try {
+        const response = await axios.get(`http://localhost:800/search/voice/${encodeURIComponent(voice_name)}`, {
+            responseType: 'json',
+            withCredentials: true
+        })
+
+        if (response) return response.data
+        
+    } catch (error) {
+        console.error(error)
+    }
+}
