ab0cf657bbff83c55a13f0866deff0fcef891e70
John Kristan <jkpogz09@gmail.com>
2024-01-30 22:37:28 +0800

    refactor: Added a new voice Type

--- server/handlers/voices.go
+++ server/handlers/voices.go
@@ -3,11 +3,13 @@ package handlers
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
+	"mime/multipart"
 	"net/http"
 	"time"
-	"fmt"
 
+	"github.com/gorilla/mux"
 	"github.com/johnkristanf/VoiceForge/server/types"
 	"github.com/johnkristanf/VoiceForge/server/utils"
 )
@@ -67,9 +69,10 @@ func (s *ApiServer) FetchVoicesHandler(res http.ResponseWriter, req *http.Reques
 		return err
 	}
 
+	search_voice := mux.Vars(req)["search_voice"] 
 	var voiceCached []*types.VoiceStruct
 
-	if cacheErr := s.client.CacheGet("voices", &voiceCached); cacheErr == nil{
+	if cacheErr := s.client.CacheGet(search_voice, &voiceCached); cacheErr == nil{
 		executionTime := time.Since(startTime)
 
 	    resMap := map[string]any{
@@ -81,13 +84,13 @@ func (s *ApiServer) FetchVoicesHandler(res http.ResponseWriter, req *http.Reques
 		return nil
 	}
 
-	voices, err := s.database.Voices() 
+	voices, err := s.database.Voices(search_voice) 
 	if err != nil{
 		return err
 	}
 	
 
-	if err := s.client.CacheSet(voices, "voices"); err != nil{
+	if err := s.client.CacheSet(voices, search_voice); err != nil{
 		return err
 	}
 
@@ -109,32 +112,41 @@ func (s *ApiServer) VoiceCloneHandler(res http.ResponseWriter, req *http.Request
 		return err
 	}
 
-	var voiceCloneData *types.VoiceCloneTypes
 	errorChan := make(chan error, 1)
+	respChan := make(chan []byte, 1)
 
-	body, err := io.ReadAll(req.Body)
-	if err != nil{
+	err := req.ParseMultipartForm(50 << 20) 
+    if err != nil {
+        return err
+    }
+
+    voice_name := req.FormValue("voice_name")
+	sample_file, _, err := req.FormFile("sample_file")
+	if err != nil {
 		return err
 	}
+	defer sample_file.Close()
 
-	if err := json.Unmarshal(body, &voiceCloneData); err != nil{
-		return err
-	}
 
 
 	wg.Add(1)
 
 	    go func() {
-		  defer wg.Done()
+		    defer wg.Done()
 
-		    if err := s.VoiceCloneRequest(voiceCloneData); err != nil{
+		    resp, err := s.VoiceCloneRequest(voice_name, sample_file); 
+			if err != nil{
 			    errorChan <- err
 		    }
 
+			fmt.Println("Response from VoiceCloneRequest:", string(resp))
+			respChan <- resp
 	    }()
+
 
 	wg.Wait()  
 	close(errorChan)
+	close(respChan)
 
 	if err := <- errorChan; err != nil{
 		return err
@@ -143,43 +155,59 @@ func (s *ApiServer) VoiceCloneHandler(res http.ResponseWriter, req *http.Request
 	return nil
 }
 
-func (s *ApiServer) VoiceCloneRequest(voiceCloneData *types.VoiceCloneTypes) error {
+func (s *ApiServer) VoiceCloneRequest(voice_name string, sample_file multipart.File) ([]byte, error) {
 
-	jsonBody, jsonErr := json.Marshal(voiceCloneData)
-	if jsonErr != nil{
-		return jsonErr
+	var requestBody bytes.Buffer
+	formWriter := multipart.NewWriter(&requestBody)
+
+	fmt.Println("Voice Name sa request", voice_name)
+	fmt.Println("Voice File  sa request", sample_file)
+
+
+	if err := formWriter.WriteField("voice_name", voice_name); err != nil {
+		return nil, err
+	}
+	
+
+	fileWriter, err := formWriter.CreateFormFile("sample_file", "sample_file")
+	if err != nil {
+		return nil, err
 	}
 
+	_, err = io.Copy(fileWriter, sample_file)
+	if err != nil {
+		return nil, err
+	}
 
+	formWriter.Close()
+
+	contentType := formWriter.FormDataContentType()
+    fmt.Println("Content-Type:", contentType)
+	
 	url := "https://api.play.ht/api/v2/cloned-voices/instant"
-	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(jsonBody))
-
-	boundary := "---011000010111000001101001"
-
-    headers := map[string]string{
-        "Accept":         "audio/mpeg",
-        "Content-Type":   "multipart/form-data; boundary=" + boundary,
-        "AUTHORIZATION":  "e1f2dd6ceaa54658a0741be57e927cb6",
-        "X-USER-ID":      "5zqbxykOY0byMItNgL7YEjPsTNz1",
-    }
-
-	for key, value := range headers{
-		req.Header.Set(key, value)
+	req, err := http.NewRequest("POST", url, &requestBody)
+	if err != nil {
+		return nil, err
 	}
+
+
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Content-Type", "multipart/form-data; boundary=---011000010111000001101001")
+	req.Header.Set("AUTHORIZATION", "e1f2dd6ceaa54658a0741be57e927cb6")
+	req.Header.Set("X-USER-ID", "5zqbxykOY0byMItNgL7YEjPsTNz1")
+
 
 	resp, err := http.DefaultClient.Do(req)
-	if err != nil{
-		return err
+	if err != nil {
+		return nil, err
 	}
-
 	defer resp.Body.Close()
-	readBody, err := io.ReadAll(resp.Body)
-	if err != nil{
-		return err
+
+
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
 	}
 
-	fmt.Println(string(readBody))
-
-	return nil
-
+	return responseBody, nil
 }
