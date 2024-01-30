6877aa4c7ef8e033290bc3eb1ad7f26f535c74a1
John Kristan <jkpogz09@gmail.com>
2024-01-30 22:23:39 +0800

    refactor: changes the tailwind import url

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 const Auth = React.lazy(() => import('./pages/Auth'))
 const TextSpeech = React.lazy(() => import("./pages/TextSpeech"))
 const VoiceCloning = React.lazy(() => import('./pages/VoiceCloning'))
+const Verification = React.lazy(() => import('./pages/Verification'))
 
 const App = () => {
 
@@ -17,6 +18,7 @@ const App = () => {
                 <Route path="/" Component={Auth} />
                 <Route path="text-speech" Component={TextSpeech} />
                 <Route path="voice-cloning" Component={VoiceCloning} />
+                <Route path="verify" Component={Verification} />
          </Routes>
 
      </Suspense>
